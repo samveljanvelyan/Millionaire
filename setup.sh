f8390c75e17a35c24269365dd0c33992f3c064a6
sjanvelyan-scdm <samvel.janvelyan@deloitte-audit-analytics.com>
2020-02-23 17:57:29 +0400

    finishing setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
+
+# Install Python 3.6 distribution.
 sudo apt-get update
 sudo apt-get install python3.6
+
+# Install pip for our Python 3.6 version.
+sudo apt-get install -y python3-pip
+
+# Install virtual environment.
 sudo apt-get install -y python3-venv
+
+# Create virtual environment.
 python3 -m venv venv
+
+# Activate virtual environment.
 source ./venv/bin/activate
+
+# Installing project requirements.
 pip install -r requirements.txt
+
+# Navigate to projects folder.
 cd ./Millionaire/
+
+# Applying models migrations and migrating to the database.
 python3 manage.py makemigrations
 python3 manage.py migrate
+
+# Filling the database with questions stored in "questions.txt".
 python3 database_connector.py
